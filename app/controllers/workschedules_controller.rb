7cf4405d251de1c6739f39d49842bd755e8538fb
Randy Tozuka <kazm1208@me.com>
2021-03-02 20:14:21 +0900

    destroy アクション作成中

--- app/controllers/workschedules_controller.rb
+++ app/controllers/workschedules_controller.rb
@@ -1,49 +1,67 @@
 class WorkschedulesController < ApplicationController
   # binding.pry
   def index
-    # @user = current_user
-    # @workschedules = Workschedule.where(user_id: current_user.id)
-    # @week_days =["日","月","火","水","木","金","土"]
+    @user = current_user
+    @workschedules = Workschedule.where(user_id: current_user.id)
+    @week_days =["日","月","火","水","木","金","土"]
   end
 
   def new
-    # @user = current_user
-    # @workschedules = Workschedule.where(user_id: current_user.id)
-    # @workschedule = Workschedule.new
-    # @statuses= Status.all
+    if current_user
+      @user = current_user
+      @workschedules = Workschedule.where(user_id: current_user.id)
+      @workschedule = Workschedule.new
+      @statuses= Status.all
+    else
+      redirect_to root_path and return
+    end
   end
 
   def create
-    # @user = current_user
-    # @date = workschedule_params[:date]
-    # # すでに予定投入済みの日に新たに予定をいれる事を防止
-    # if Workschedule.where(user_id: @user.id).where(date: @date).any?
-    #   flash[:danger]= "その日はすでに予定投入済みです"
-    #   redirect_to root_path and return
-    # end
-    # if Workschedule.create(workschedule_params)
-    #   flash[:success]= "登録できました"
-    #   redirect_to root_path and return
-    # else
-    #   flash[:danger]= "登録できませんでした"
-    #   redirect_to '/workschedules/new' and return
-    # end
+    @user = current_user
+    @date = workschedule_params[:wdate]
+    # すでに予定投入済みの日に新たに予定をいれる事を防止
+    if Workschedule.where(user_id: @user.id).where(wdate: @date).any?
+      flash[:danger]= "その日はすでに予定投入済みです"
+      redirect_to root_path and return
+    end
+    if Workschedule.create(workschedule_params)
+      flash[:success]= "登録できました"
+      redirect_to root_path and return
+    else
+      flash[:danger]= "登録できませんでした"
+      redirect_to '/workschedules/new' and return
+    end
   end
 
   def edit
-    # @user = current_user
-    # @statuses= Status.all
-    # binding.pry
-    # @workschedule = Workschedule.find_by(params[:id])
+    @user = current_user
+    @statuses= Status.all
+    @workschedule = Workschedule.find(params[:id])
   end
 
   def update
+    @user = current_user
+    @date = workschedule_params[:wdate]
+    @workschedule = Workschedule.find(params[:id])
+    # binding.pry
+    if @workschedule.update(workschedule_params)
+      flash[:success]= "編集できました"
+      redirect_to root_path and return
+    else
+      flash[:danger]= "編集できませんでした"
+      redirect_to new_workschedule_path and return
+    end
+  end
 
+  def destroy
+    @workschedule = Workschedule.find(params[:id])
+    @workschedule.destroy
   end
 
   private
-      # def workschedule_params
-      #   params.require(:workschedule).permit(:date, :status_id).merge(user_id: current_user.id)
-      # end
+      def workschedule_params
+        params.require(:workschedule).permit(:wdate, :status_id).merge(user_id: current_user.id)
+      end
 
 end
